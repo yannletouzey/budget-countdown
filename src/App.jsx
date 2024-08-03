f0b1f681985d26f0cde348f6556b120590facd63
yannletouzey <y.letouzey@gmail.com>
2024-08-03 12:29:46 +0200

    fisrt commit + add budget form

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SumForm from './Form';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Monthly Budget</h1>
+      <SumForm />
+    </div>
+  )
+}
